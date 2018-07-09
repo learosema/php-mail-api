af2bf1933cc9195ae35dfd3cefb596b830001f3f
Lea Rosema <terabaud@gmail.com>
2018-07-09 21:23:01 +0200

    add route

--- src/routes.php
+++ src/routes.php
@@ -4,11 +4,17 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 
 // Routes
-
+/*
 $app->get('/[{name}]', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
+}); */
+
+$app->post('/mail', function (Request $request, Response $response, array $args) {
+    $this->logger->info("'/mail' route");
+    $recaptcha = new ReCaptcha\ReCaptcha("todo_where_to_put_my_secret_key");
+    return $response->write("It works!");
 });
