7f5911af56a532f73f5f7daae8c53c4e27b6ba73
Lea Rosema <terabaud@gmail.com>
2018-07-14 16:00:22 +0200

    change route to post

--- src/routes.php
+++ src/routes.php
@@ -10,13 +10,16 @@ $app->get('/', function(Request $request, Response $response) {
     return $response->withStatus(301)->withHeader('Location', '/docs/');
 });
 
-$app->get('/mail', function (Request $request, Response $response, array $args) {
+$app->post('/mail', function (Request $request, Response $response, array $args) {
     $this->logger->info("'/mail' route");
     $appSettings = $this->get('settings')['appSettings'];
     $secret = $appSettings['recaptcha_secret'];
     $recaptcha = new ReCaptcha\ReCaptcha($secret);
+    $parsedBody = $request->getParsedBody();
+    var_dump($parsedBody);
+
+
     return $response->write("It works!");
-    
 })->add(new \App\Middleware\RateLimit($container, 3));
 
 $app->options('/mail', function ($request, $response, $args) {
