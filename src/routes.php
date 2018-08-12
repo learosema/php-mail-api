7fb2f93f3db4f1ee3f4174e501f61996191dd525
Lea Rosema <terabaud@gmail.com>
2018-08-12 21:22:10 +0200

    update

--- src/routes.php
+++ src/routes.php
@@ -10,15 +10,24 @@ $app->get('/', function(Request $request, Response $response) {
     return $response->withStatus(301)->withHeader('Location', '/docs/');
 });
 
-$app->get('/mail', function (Request $request, Response $response, array $args) {
+$app->post('/mail', function (Request $request, Response $response, array $args) {
     $this->logger->info("'/mail' route");
     $appSettings = $this->get('settings')['appSettings'];
     $secret = $appSettings['recaptcha_secret'];
     $recaptcha = new ReCaptcha\ReCaptcha($secret);
-
+    $params = $request->getParsedBody();
+    $requiredParams = array('subject', 'message', 'email');
+    foreach ($requiredParams as $reqParam) {
+        if (! array_key_exists($reqParam, $params) || strlen(trim($params[$reqParam])) < 2 ) {
+            return $response->withStatus(400)->write("Parameter $reqParam missing");
+        }
+    }
+    // $name = "$appSettings['name'] < $appSettings['email'] >";
+    mail($appSettings['mailto'], $params['subject'], $params['message']);
     return $response->write("It works!");
 });
 
 $app->options('/mail', function ($request, $response, $args) {
     return $response;
 });
+
